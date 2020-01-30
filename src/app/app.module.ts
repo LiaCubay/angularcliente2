e838317c6ce2ffa252d920e75fa0b3e1aa9aa69c
LiaCubay <cubay82@gmail.com>
2020-01-30 18:55:04 +0100

    3 files modified, 1 added, 10 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,20 @@
+//Modulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {RouterModule, Routes} from '@angular/router';
-import {APP_ROUTES} from './app.routes';
+//Componentes
 import {AppComponent} from './app.component';
-import {HelloComponent} from './hello.component';
-import { ClienteBuscadorComponent } from './cliente/cliente-buscador/cliente-buscador.component';
-import {PageNotFoundComponent} from './pageNotFound/page-not-found/page-not-found.component';
-import { InsideViewComponent } from './views/inside-view/inside-view.component';
+import {HelloComponent} from './componentes/hello/hello.component';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule,
-    RouterModule.forRoot(APP_ROUTES, {useHash:true})
   ],
   declarations: [ 
     AppComponent, 
     HelloComponent, 
-    ClienteBuscadorComponent,
-    PageNotFoundComponent,
-    InsideViewComponent,
   ],
   bootstrap:    [ AppComponent ]
 })
---
+++ src/app/componentes/hello/hello.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template: `<h1>Hello {{name}}!</h1><button (click)="hazHalgo()">Púlsame</button>`,
+  styles: [`h1 { font-family: Lato; }`]
+})
+export class HelloComponent  {
+  @Input('nameOut') name: string;
+  @Output() decirAlgo = new EventEmitter;
+
+  hazHalgo(): any{
+    alert('Estoy haciendo algo');
+    this.decirAlgo.emit('Estoy emitendo!!');
+  }
+}
